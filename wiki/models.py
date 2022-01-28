31d810dd5d4c6b5c0c09bf52f945c029705047ed
resolt <pdmmichaelsen@gmail.com>
2022-01-28 19:35:42 +0100

    still not working

--- wiki/models.py
+++ wiki/models.py
@@ -5,10 +5,18 @@ from django.urls import reverse
 class WikiPage(models.Model):
     title = models.CharField(max_length=100)
     text = models.TextField(max_length=2000)
-    links = models.JSONField(default=dict)
+    links = models.ManyToManyField("wiki.WikiPage", verbose_name="Links")
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse("wiki:detail", kwargs={'pk':self.pk})
+
+    @property
+    def url(self):
+        return self.get_absolute_url()
+
+    @property
+    def get_links(self):
+        return self.links.all()
