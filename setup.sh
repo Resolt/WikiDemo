a2c4f3d28084bb55a4d2d0c89e6353a5c2555492
resolt <pdmmichaelsen@gmail.com>
2022-01-29 14:45:03 +0100

    split to dev and prod docker-compose files

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 
 : ${TAG=wiki-demo}
 docker build . --file Dockerfile --tag $TAG
-docker-compose -f docker-compose.yml --env-file ./docker-compose.env up -d
+docker-compose -f docker-compose.prod.yml --env-file ./docker-compose.env up -d
